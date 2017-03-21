3b1c64f5802e1c6e22f45c5ef050a76945f5e2ff
Edward Goo <Edwardx89@yahoo.com>
2017-03-21 16:07:21 -0400

    added hotel add function

--- starting-point/public/js/index.js
+++ starting-point/public/js/index.js
@@ -1,6 +1,17 @@
 console.log("running index.js")
 
-$('#itinerary').on('click','.itinerary-item',function(){
-$(this).remove();
+var div = function (title){
+  return (' <div class="itinerary-item"><span class="title">'+title+'</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>')
+}
+
+$('#itinerary').on('click', 'button', function () {
+  ($(this).parent().remove())
 });
 
+$('#add-hotel').on('click', function () {
+  if ($('#my-hotel').children().length > 0) {
+    alert('You already selected a hotel');
+  } else {
+    $('#my-hotel').append(div($('#hotel-choices').val()))
+  }
+});
